bcc315a21a20f3740db2d75c2363ba2ce6adae40
Jasmine Ulloa <ulloajazz6@gmail.com>
2022-12-22 19:53:30 -0800

    commenting code snippets

--- script.js
+++ script.js
@@ -1,36 +1,23 @@
 // Assignment code here
+
+//setting my character variables to contain all the letters, numbers, and characters 
 var alphaLower = 'abcdefghijklmnopqrstuvwxyz';
 var alphaUpper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 var numbers = '0123456789';
 var specialChar = '~`!@#$%^&*()_-+={[}]|:;">/';
 
-
-//I want the user to confirm or deny lowercase  -- confirm("Include lowercase?");
-//I want the user to confirm or deny uppercase -- confirm("Include uppercase?");
-//I want the user to confirm or deny numbers -- confirm("Include numbers?");
-
-// var confirmUpper = window.confirm("Do you want to include uppercase letters?")
-
-//if the user denies all criteria, i want to alert("must choose characters")
-
-//I want to check if the user has clicked yes or no to each criteria
-// if(alphaUpper && alphaLower && numbers && specialChar){generate a password with all criteria}
-// if(alphaLower && numbers && specialChar){generate a password with the 3 criteria}
-// if(numbers && specialChar){generate a password with 2 criteria}
-// if(specialChar){generate a password with 1 criteria}
-
-//i want to set the generate password to the existing password variable on line 59
-
-//after user clicks thru confirms i want the computer to generate a random password
-//i want to display that password to the screen
-
-
-
-function generatePassword(){
+//
+function generatePassword(passwordText){
+  /*setting passwordChoices to an empty string because i am going to change 
+  this variable later to include character criteria*/
   var passwordChoices = ''
   var password =''
+  
+  //Prompting the user to give me a number
   var passwordPrompt = window.prompt("Please enter number of characters", "");
   
+  //parse the string to an integer and testing that the number meets min of 8 and max of 128. if not, reprompt user
+  //if user clicks cancels, alert the user
   if( parseInt(passwordPrompt) < 8 || parseInt(passwordPrompt) > 128){
     window.alert("Please choose a number between 8-128")
     generatePassword()
@@ -38,23 +25,39 @@ function generatePassword(){
     window.alert("Goodbye!")
   }
 
-  var isUppercase = window.confirm("Do you need Uppercase?")
+  var isUppercase = window.confirm("Do you want Uppercase?")
+  var isLowercase = window.confirm("Do you want Lowercase?")
+  var isNumbers = window.confirm("Do you want Numbers?")
+  var isSpecialChar = window.confirm("Do you want special characters?")
 
   if(isUppercase){
-    passwordChoices= passwordChoices + alphaUpper
+    passwordChoices = passwordChoices + alphaUpper
   }
+  if (isLowercase){
+    passwordChoices = passwordChoices + alphaUpper + alphaLower
+  }
+  if (isNumbers){
+    passwordChoices = passwordChoices + alphaUpper + alphaLower + numbers
+  }
+  if (isSpecialChar){
+    passwordChoices = passwordChoices + alphaUpper + alphaLower + numbers + specialChar
+  }
+
   for (let index = 0; index < parseInt(passwordPrompt); index++) {
     const randomElement = passwordChoices[Math.floor(Math.random() * passwordChoices.length)];
     password = password + randomElement
+  } 
+ if (!isUppercase && !isLowercase){
+    alert("Please choose at least 1 criteria. Try again")
+    passwordText.value = ""
   }
-  
-  return password
+  return password;
+
 
 } 
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
-
 
 
 // Write password to the #password input
